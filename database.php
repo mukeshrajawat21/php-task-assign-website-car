9942edcd53a4b410542b17fdb8c20045fb89a29b
mukeshrajawat21 <mukeshrajawat6@gmail.com>
2026-01-21 16:47:59 +0530

    Database changes

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect("localhost", "root", "", "cars_website");
 
   if(!$conn)
     {
-        echo "Database is not connected";
+        echo " Database is not connected !";
     }
 
 ?>
